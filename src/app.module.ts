bdfdafd9142fe79f530ca4af901669217d4b8254
Brandon Castillo <brajoel98@gmail.com>
2023-12-27 14:16:17 -0800

    feat(app): :sparkles: CRUD items

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { join } from 'path';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { join } from 'path';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    ItemsModule,
   ],
   controllers: [],
   providers: [],
